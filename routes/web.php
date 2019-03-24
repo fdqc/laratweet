f9d9453970bd50e268ca2ef82a1769b025aeb200
QFernando <f.quintana88@yahoo.com.ar>
2019-03-24 18:03:11 -0300

    Route to store a new comment

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+// Comments routes
+Route::post('comments/store','CommentsController@store')->name('comments/store');
