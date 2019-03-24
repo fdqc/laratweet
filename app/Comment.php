f3a32273e032123e9b528206efad60f1ea58e6c9
QFernando <f.quintana88@yahoo.com.ar>
2019-03-24 14:14:07 -0300

    Created an accesor for created_at property

--- app/Comment.php
+++ app/Comment.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Comment extends Model
 {
-    //
+  /**
+     * Get the comment's formated created_at
+     * value.
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getCreatedAtAttribute($value)
+    {
+      $date = new \DateTime($value);
+      return $date->format('D M d H:i:s O Y');
+    }
 }
