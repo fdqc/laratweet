dfcd677b4cfcf36ecb0cf25ed8ee53b897b911fc
QFernando <f.quintana88@yahoo.com.ar>
2019-03-24 02:52:35 -0300

    Migration for creating replies table

---
+++ database/migrations/2019_03_24_044022_create_replies_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRepliesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('replies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('comment_id')->unsigned();
+            $table->integer('reply_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('comment_id')->references('id')->on('comments')->onDelete('cascade');
+            $table->foreign('reply_id')->references('id')->on('comments')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('replies');
+    }
+}
