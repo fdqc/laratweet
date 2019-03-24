d66ed97b375b90ba41ccb87a7a2fa18660e1a22f
QFernando <f.quintana88@yahoo.com.ar>
2019-03-24 15:25:17 -0300

    I define two methods: one to get an instance where the comment is a reply and the other to get the user who created a comment

--- app/Comment.php
+++ app/Comment.php
@@ -18,4 +18,28 @@ class Comment extends Model
       $date = new \DateTime($value);
       return $date->format('D M d H:i:s O Y');
     }
+
+  /**
+   * Gets a reply instance where
+   * reply_id is the replying comment's id
+   * and comment_id is the replied comment's
+   * id.
+   *
+   * @return Reply instance
+   */
+    public function reply()
+    {
+      return $this->hasOne('App\Reply','reply_id');
+    }
+
+  /**
+   * Returns the user who created
+   * the comment.
+   *
+   * @return User instance
+   */
+    public function user()
+    {
+      return $this->belongsTo('App\User');
+    }
 }
