1dfc4f6135c9eae8af023eb50a73172b18fa2a1a
QFernando <f.quintana88@yahoo.com.ar>
2019-03-25 05:22:10 -0300

    Followers create table migration

---
+++ database/migrations/2019_03_25_071902_create_followers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFollowersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('followers', function (Blueprint $table) {
+          $table->integer('user_id')->unsigned();
+          $table->integer('follower_id')->unsigned();
+          $table->primary(['user_id','follower_id']);
+          $table->timestamps();
+
+          $table->foreign('user_id')->references('id')->on('users');
+          $table->foreign('follower_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('followers');
+    }
+}
